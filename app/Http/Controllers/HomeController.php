977447481739b4967aa0f35fecfbbe29b6b0150a
Artur Rossi <arturossi@github.com>
2022-02-25 13:45:36 -0300

    Edição do dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,8 @@ use App\Models\Obras;
 
 use Illuminate\Http\Request;
 
+use Carbon\Carbon;
+
 class HomeController extends Controller
 {
     /**
@@ -32,20 +34,28 @@ class HomeController extends Controller
         $obras_total = Obras::select('id')->count('id');
 
         $obras = Obras::select('obras.id', 'titulo_obra', 'tesauro_id', 'titulo_tesauro', 'acervo_id', 'nome_acervo', 'material_id_1', 'm1.titulo_material as titulo_material_1', 'material_id_2', 'm2.titulo_material as titulo_material_2', 'material_id_3', 'm3.titulo_material as titulo_material_3', 'foto_frontal_obra', 'obras.seculo_id', 'titulo_seculo')
-        ->join('seculos as s', 's.id', '=', 'obras.seculo_id')
-        ->join('tesauros as t', 't.id', '=', 'tesauro_id')
-        ->join('acervos as a', 'a.id', '=', 'acervo_id')
-        ->leftjoin('materiais as m1', 'm1.id', '=', 'material_id_1')
-        ->leftjoin('materiais as m2', 'm2.id', '=', 'material_id_2')
-        ->leftjoin('materiais as m3', 'm3.id', '=', 'material_id_3')
-        ->orderBy('obras.id', 'DESC')
-        ->limit(10)
-        ->get();
-
+            ->join('seculos as s', 's.id', '=', 'obras.seculo_id')
+            ->join('tesauros as t', 't.id', '=', 'tesauro_id')
+            ->join('acervos as a', 'a.id', '=', 'acervo_id')
+            ->leftjoin('materiais as m1', 'm1.id', '=', 'material_id_1')
+            ->leftjoin('materiais as m2', 'm2.id', '=', 'material_id_2')
+            ->leftjoin('materiais as m3', 'm3.id', '=', 'material_id_3')
+            ->orderBy('obras.id', 'DESC')
+            ->limit(10)
+            ->get();
         $obras = $obras->reverse();
+        
+        // Estatísticas dos dias
+        $days = ['1', '7', '30'];
 
-       // print_r($usuarios);die;
+        $estatisticasObra = [];
+        $estatisticasAcervo = [];
 
-        return view('home', ['usuarios_total' => $usuarios_total, 'acervos_total' => $acervos_total, 'obras_total' => $obras_total, 'obras' => $obras]);
+        foreach ($days as $key => $day) {
+            $estatisticasObra[$day] = Obras::where('created_at','>=', Carbon::now()->subdays($day))->count('id');
+            $estatisticasAcervo[$day] = Acervos::where('created_at','>=', Carbon::now()->subdays($day))->count('id');
+        }
+
+        return view('home', ['usuarios_total' => $usuarios_total, 'acervos_total' => $acervos_total, 'obras_total' => $obras_total, 'obras' => $obras, 'estatisticasObra' => $estatisticasObra, 'estatisticasAcervo' => $estatisticasAcervo]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,20 +6,23 @@
 @section('content')
 <section class="section">
           <div class="row ">
-            <div class="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <div class="col-xl-2 col-lg-6 col-md-6 col-sm-6 col-xs-12">
               <div class="card">
                 <div class="card-statistic-4">
                   <div class="align-items-center justify-content-between">
                     <div class="row ">
-                      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                      <div class="col-lg-7 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
                         <div class="card-content">
                           <h5 class="font-15">Usuários</h5>
                           <h2 class="mb-3 font-18">{{ $usuarios_total }}</h2>
+                          @foreach ($estatisticasAcervo as $key => $estatisticaAcervo)
+                            <p class="mb-0">&nbsp;</p>
+                          @endforeach
                         </div>
                       </div>
-                      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                      <div class="col-lg-5 col-md-6 col-sm-6 col-xs-6 pl-0">
                         <div class="banner-img">
-                          <img src="assets/img/banner/1.png" alt="">
+                          <img style="max-width:140px; max-height:140px;" src="assets/img/banner/usuarios.svg" alt="">
                         </div>
                       </div>
                     </div>
@@ -27,21 +30,33 @@
                 </div>
               </div>
             </div>
-            <div class="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <div class="col-xl-5 col-lg-6 col-md-6 col-sm-6 col-xs-12">
               <div class="card">
                 <div class="card-statistic-4">
                   <div class="align-items-center justify-content-between">
                     <div class="row ">
-                      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                      <div class="col-lg-7 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
                         <div class="card-content">
                           <h5 class="font-15">Acervos</h5>
                           <h2 class="mb-3 font-18">{{ $acervos_total }}</h2>
-                          <p class="mb-0"><span class="col-orange">09%</span> Decrease</p>
+                            @foreach ($estatisticasAcervo as $key => $estatisticaAcervo)
+                              <p class="mb-0">
+                              @if($estatisticaAcervo > 0)
+                              <span class="col-green">{{ $estatisticaAcervo }}</span> {{ $estatisticaAcervo > 1 ? 'acervos foram cadastrados' : 'acervo foi cadastrado' }} {{ $key > 1 ? 'nos últimos ' . $key . ' dias' : 'no último dia' }}.
+                              @else
+                                @if($estatisticaAcervo < 0)
+                                <span class="col-orange">{{ $estatisticaAcervo }}</span> {{ $estatisticaAcervo < 1 ? 'acervos foram descadastrados' : 'acervo foi descadastrado' }} {{ $key > 1 ? 'nos últimos ' . $key . ' dias' : 'no último dia' }}.
+                                @else
+                                Nenhum acervo cadastrado {{ $key > 1 ? 'nos últimos ' . $key . ' dias' : 'no último dia' }}.
+                                @endif
+                              @endif
+                              </p>
+                            @endforeach
                         </div>
                       </div>
-                      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                      <div class="col-lg-5 col-md-6 col-sm-6 col-xs-6 pl-0">
                         <div class="banner-img">
-                          <img src="assets/img/banner/2.png" alt="">
+                          <img style="max-width:140px; max-height:140px;" src="assets/img/banner/acervos.svg" alt="">
                         </div>
                       </div>
                     </div>
@@ -49,22 +64,33 @@
                 </div>
               </div>
             </div>
-            <div class="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <div class="col-xl-5 col-lg-6 col-md-6 col-sm-6 col-xs-12">
               <div class="card">
                 <div class="card-statistic-4">
                   <div class="align-items-center justify-content-between">
                     <div class="row ">
-                      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                      <div class="col-lg-7 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
                         <div class="card-content">
                           <h5 class="font-15">Obras</h5>
                           <h2 class="mb-3 font-18">{{ $obras_total }}</h2>
-                          <p class="mb-0"><span class="col-green">18%</span>
-                            Increase</p>
+                          @foreach ($estatisticasObra as $key => $estatisticaObra)
+                              <p class="mb-0">
+                              @if($estatisticaObra > 0)
+                              <span class="col-green">{{ $estatisticaObra }}</span> {{ $estatisticaObra > 1 ? 'obras foram cadastradas' : 'obra foi cadastrada' }} {{ $key > 1 ? 'nos últimos ' . $key . ' dias' : 'no último dia' }}.
+                              @else
+                                @if($estatisticaObra < 0)
+                                <span class="col-orange">{{ $estatisticaObra }}</span> {{ $estatisticaObra < 1 ? 'obras foram descadastradas' : 'obra foi descadastrada' }} {{ $key > 1 ? 'nos últimos ' . $key . ' dias' : 'no último dia' }}.
+                                @else
+                                Nenhuma obra cadastrada {{ $key > 1 ? 'nos últimos ' . $key . ' dias' : 'no último dia' }}.
+                                @endif
+                              @endif
+                              </p>
+                            @endforeach
                         </div>
                       </div>
-                      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                      <div class="col-lg-5 col-md-6 col-sm-6 col-xs-6 pl-0">
                         <div class="banner-img">
-                          <img src="assets/img/banner/3.png" alt="">
+                          <img style="max-width:140px; max-height:140px;" src="assets/img/banner/obras.svg" alt="">
                         </div>
                       </div>
                     </div>
@@ -78,21 +104,14 @@
               <div class="card">
                 <div class="card-header">
                   <h4>Últimas 10 obras cadastradas</h4>
-                  <div class="card-header-form">
-                    <form>
-                      <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Search">
-                        <div class="input-group-btn">
-                          <button class="btn btn-primary"><i class="fas fa-search"></i></button>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
+                  <div class="card-header-form"></div>
                 </div>
                 <div class="card-body p-0">
                   <div class="table-responsive">
                     <table class="table table-striped">
                       <tr>
+                        <th style="padding-left:25px; text-align: center;">Id</th>
+                        <th style="text-align: center;">Foto Principal</th>
                         <th>Título</th>
                         <th>Tesauro</th>
                         <th>Acervo</th>
