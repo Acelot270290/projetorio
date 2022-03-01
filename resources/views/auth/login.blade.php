5db727c7fac4afceb9435b179b1e48053964749f
Artur Rossi <arturossi@github.com>
2022-03-01 12:28:44 -0300

    Modificação no footer no login para sempre ficar em baixo.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -70,13 +70,4 @@
         </div>
     </div>
 </div>
-<style>
-    #footer {
-   position:absolute;
-   bottom:0;
-   width:100%;
-   height:60px;   /* Height of the footer */
-   background:#6cf;
-}
-</style>
 @endsection
