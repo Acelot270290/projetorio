47b3cf58ab95422c60bc4fbe0a0eb0eb4a49f3b5
Alan Diniz <adiniz@girafa.com.br>
2022-05-07 10:15:20 -0300

    teste do mkdir

--- app/Http/Controllers/ObraController.php
+++ app/Http/Controllers/ObraController.php
@@ -647,7 +647,7 @@ class ObraController extends Controller
         if (!is_dir($preBasePath)) {
             // Ele será criado
            // mkdir(public_path($preBasePath));
-            mkdir(public_path($preBasePath), 0755, true);
+            mkdir(public_path($preBasePath), 0755, true,true);
             // E o subfolder também (se o pré não existe, seus filhos também não existem)
             mkdir(public_path($basePath));
         }else if (!is_dir($basePath)) {
