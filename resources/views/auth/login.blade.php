02cd5cdddf97b58973171e784b0f7c4e0f7699cb
Alan <you@example.com>
2022-09-01 16:00:26 -0300

    Verificando caso o user esteja logado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,7 +44,7 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Lembrar') }}
+                                        {{ __('Manter Conectado') }}
                                     </label>
                                 </div>
                             </div>
